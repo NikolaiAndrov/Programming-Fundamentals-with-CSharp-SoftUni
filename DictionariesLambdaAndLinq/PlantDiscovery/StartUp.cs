68fe30a7ffc8baf16682b494b3c3610395afa78a
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2023-03-13 23:59:58 +0200

    Update StartUp.cs

--- DictionariesLambdaAndLinq/PlantDiscovery/StartUp.cs
+++ DictionariesLambdaAndLinq/PlantDiscovery/StartUp.cs
@@ -1,59 +1,71 @@
-﻿public class StartUp
+public class StartUp
 {
     public static void Main()
     {
         var plants = new Dictionary<string, List<int>>();
         AddPlants(plants);
-
-        string input;
-
-        while ((input = Console.ReadLine()) != "Exhibition")
-        {
-            var args = input.Split(new char[] { ' ', ':', '-' }, StringSplitOptions.RemoveEmptyEntries);
-            var command = args[0];
-            var plant = args[1];
-
-            if (!plants.ContainsKey(plant))
-            {
-                Console.WriteLine("error");
-                continue;
-            }
-
-            if (command == "Rate")
-            {
-                var rating = int.Parse(args[2]);
-                plants[plant].Add(rating);
-            }
-            else if (command == "Update")
-            {
-                var rarity = int.Parse(args[2]);
-                plants[plant][0] = rarity;
-            }
-            else if (command == "Reset")
-            {
-                if (plants[plant].Count > 1)
-                {
-                    plants[plant].RemoveRange(1, plants[plant].Count - 1);
-                }
-            }
-        }
-
+        ModifyPlants(plants);
         PrintPlants(plants);
     }
+
     static void PrintPlants(Dictionary<string, List<int>> plants)
     {
         Console.WriteLine("Plants for the exhibition:");
 
         foreach (var plant in plants)
         {
-            var avg = 0.0;
+            var avg = 0d;
 
-            if (plants[plant.Key].Count > 1)
+            if (plant.Value.Count > 1)
             {
-                avg = plants[plant.Key].Skip(1).Take(plants[plant.Key].Count - 1).Average();
+                avg = plant.Value.Skip(1).Average();
             }
-            
-            Console.WriteLine($"- {plant.Key}; Rarity: {plants[plant.Key][0]}; Rating: {avg:F2}");
+
+            Console.WriteLine($"- {plant.Key}; Rarity: {plant.Value[0]}; Rating: {avg:F2}");
+        }
+    }
+    static void ModifyPlants(Dictionary<string, List<int>> plants)
+    {
+        string input;
+
+        while ((input = Console.ReadLine()) != "Exhibition")
+        {
+            var args = input.Split(new char[] {':', ' ', '-'}, StringSplitOptions.RemoveEmptyEntries);
+            var command = args[0];
+            var plant = args[1];
+
+            if (command == "Rate")
+            {
+                if (!plants.ContainsKey(plant))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                var rating = int.Parse(args[2]);
+                plants[plant].Add(rating);
+            }
+            else if (command == "Update")
+            {
+                if (!plants.ContainsKey(plant))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                var rarity = int.Parse(args[2]);
+                plants[plant][0] = rarity;
+            }
+            else if (command == "Reset")
+            {
+                if (!plants.ContainsKey(plant))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                plants[plant].RemoveRange(1, plants[plant].Count - 1);
+            }
         }
     }
     static void AddPlants(Dictionary<string, List<int>> plants)
@@ -62,9 +74,9 @@
 
         for (int i = 0; i < n; i++)
         {
-            var args = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
-            var plant = args[0];
-            var rarity = int.Parse(args[1]);
+            var plantInfo = Console.ReadLine().Split("<->");
+            var plant = plantInfo[0];
+            var rarity = int.Parse(plantInfo[1]);
 
             if (!plants.ContainsKey(plant))
             {
@@ -74,5 +86,6 @@
 
             plants[plant][0] = rarity;
         }
+        
     }
 }
