f7498ff05b973b6ebe8b1569c0fa3a512369cff5
NikolaiAndrov <32267207+NikolaiAndrov@users.noreply.github.com>
2023-03-19 14:06:03 +0200

    Update StartUp.cs

--- Exams/P!rates/StartUp.cs
+++ Exams/P!rates/StartUp.cs
@@ -1,58 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Collections.Generic;
 public class StartUp
 {
     public static void Main()
     {
-        var citiesAndResources = new Dictionary<string, List<int>>();
-        AddCitiesAndResources(citiesAndResources);
-
-        string input;
-
-        while ((input = Console.ReadLine()) != "End")
-        {
-            var args = input.Split("=>", StringSplitOptions.RemoveEmptyEntries);
-            var command = args[0];
-            var town = args[1];
-
-            if (command == "Plunder")
-            {
-                var people = int.Parse(args[2]);
-                var gold = int.Parse(args[3]);
-                Console.WriteLine($"{town} plundered! {gold} gold stolen, {people} citizens killed.");
-                Plunder(citiesAndResources, town, people, gold);
-            }
-            else if (command == "Prosper")
-            {
-                var gold = int.Parse(args[2]);
-
-                if (gold < 0)
-                {
-                    Console.WriteLine("Gold added cannot be a negative number!");
-                    continue;
-                }
-
-                if (citiesAndResources.ContainsKey(town))
-                {
-                    citiesAndResources[town][1] += gold;
-                    Console.WriteLine($"{gold} gold added to the city treasury. {town} now has {citiesAndResources[town][1]} gold.");
-                }
-            }
-        }
-
-        PrintCitiesAndResources(citiesAndResources);
+        var cities = new Dictionary<string, List<int>>();
+        AddCities(cities);
+        GetCommands(cities);
+        PrintCities(cities);
     }
 
-    static void PrintCitiesAndResources(Dictionary<string, List<int>> citiesAndResources)
+    static void PrintCities(Dictionary<string, List<int>> cities)
     {
-        if (citiesAndResources.Count > 0)
+        if (cities.Count > 0)
         {
-            Console.WriteLine($"Ahoy, Captain! There are {citiesAndResources.Count} wealthy settlements to go to:");
+            Console.WriteLine($"Ahoy, Captain! There are {cities.Count} wealthy settlements to go to:");
 
-            foreach (var item in citiesAndResources)
+            foreach (var city in cities)
             {
-                Console.WriteLine($"{item.Key} -> Population: {item.Value[0]} citizens, Gold: {item.Value[1]} kg");
+                Console.WriteLine($"{city.Key} -> Population: {city.Value[0]} citizens, Gold: {city.Value[1]} kg");
             }
         }
         else
@@ -60,22 +27,47 @@ public class StartUp
             Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
         }
     }
-    static void Plunder(Dictionary<string, List<int>> citiesAndResources, string town, int people, int gold)
+    static void GetCommands(Dictionary<string, List<int>> cities)
     {
-        if (citiesAndResources.ContainsKey(town))
-        {
-            citiesAndResources[town][0] -= people;
-            citiesAndResources[town][1] -= gold;
+        string input;
 
-            if (citiesAndResources[town][0] <= 0 || citiesAndResources[town][1] <= 0)
+        while ((input = Console.ReadLine()) != "End")
+        {
+            var args = input.Split("=>", StringSplitOptions.RemoveEmptyEntries);
+            var command = args[0];
+            var city = args[1];
+
+            if (command == "Plunder")
             {
-                citiesAndResources.Remove(town);
-                Console.WriteLine($"{town} has been wiped off the map!");
+                var people = int.Parse(args[2]);
+                var gold = int.Parse(args[3]);
+
+                cities[city][0] -= people;
+                cities[city][1] -= gold;
+                Console.WriteLine($"{city} plundered! {gold} gold stolen, {people} citizens killed.");
+
+                if (cities[city][0] <= 0 || cities[city][1] <= 0)
+                {
+                    cities.Remove(city);
+                    Console.WriteLine($"{city} has been wiped off the map!");
+                }
+            }
+            else if (command == "Prosper")
+            {
+                var gold = int.Parse(args[2]);
+
+                if (gold < 0)
+                {
+                    Console.WriteLine($"Gold added cannot be a negative number!");
+                    continue;
+                }
+
+                cities[city][1] += gold;
+                Console.WriteLine($"{gold} gold added to the city treasury. {city} now has {cities[city][1]} gold.");
             }
         }
-
     }
-    static void AddCitiesAndResources(Dictionary<string, List<int>> citiesAndResources)
+    static void AddCities(Dictionary<string, List<int>> cities)
     {
         string input;
 
@@ -86,15 +78,15 @@ public class StartUp
             var population = int.Parse(args[1]);
             var gold = int.Parse(args[2]);
 
-            if (!citiesAndResources.ContainsKey(city))
+            if (!cities.ContainsKey(city))
             {
-                citiesAndResources[city] = new List<int>();
-                citiesAndResources[city].Add(0);
-                citiesAndResources[city].Add(0);
+                cities[city] = new List<int>();
+                cities[city].Add(0);
+                cities[city].Add(0);
             }
 
-            citiesAndResources[city][0] += population;
-            citiesAndResources[city][1] += gold;
+            cities[city][0] += population;
+            cities[city][1] += gold;
         }
     }
 }
